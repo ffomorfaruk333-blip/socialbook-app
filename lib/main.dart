366b69929c4359b9d4fc988c87ba760225c3addb
ffomorfaruk333-blip <ffomorfaruk333@gmail.com>
2026-08-02 22:29:47 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ Future<void> main() async {
 
   await Supabase.initialize(
     url: supabaseUrl,
-    anonKey: supabasePublishableKey,
+supabasePublishableKey,
   );
 
   runApp(const SocialBookApp());
